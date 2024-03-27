1e25ea3355c9eaf7268c19740936c7aadd782be7
Sam Uherek <samuherekbiz@gmail.com>
2024-03-27 10:14:11 +0100

    imple player and moving

--- src/map.rs
+++ src/map.rs
@@ -30,12 +30,28 @@ impl Map {
                 match self.tiles[idx] {
                     TileType::Floor => {
                         ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
-                    },
+                    }
                     TileType::Wall => {
                         ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
                     }
                 }
             }
+        }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
         }
     }
 }
